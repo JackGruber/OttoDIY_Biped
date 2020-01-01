b8062805d5dbfaebcf35b2f08d2f1d7841803131
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-01-01 13:02:20 +0100

    Add Bibed Class

---
+++ include/io_pins.h
@@ -0,0 +1,4 @@
+#define PIN_LEFT_LEG 4 //servo[0]  left leg
+#define PIN_RIGHT_LEG 6 //servo[1]  right leg
+#define PIN_LEFT_FOOT 5 //servo[2]  left foot
+#define PIN_RIGHT_FOOT 7 //servo[3]  right foot
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
+#include <biped.h>
+#include <io_pins.h>
+
+Biped Walker(PIN_LEFT_LEG, PIN_RIGHT_LEG, PIN_LEFT_FOOT, PIN_RIGHT_FOOT);
 
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Starting Biped ..."));
+  Walker.Home();
 }
 
 void loop() {
